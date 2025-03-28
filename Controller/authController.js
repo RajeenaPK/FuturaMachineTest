3af25c31b7b26700640b67c192d986795e865487
RajeenaPK <rajeenafazil10@gmail.com>
2025-03-28 16:38:38 +0530

    password-reset-correction

--- Controller/authController.js
+++ Controller/authController.js
@@ -69,20 +69,31 @@ exports.resetPassword = async (req, res) => {
   try {
     const { token } = req.params;
     const { password } = req.body;
-    console.log(token,password);
+    
+    console.log("Received Token:", token);
+    
     const user = await User.findOne({ resetPasswordExpire: { $gt: Date.now() } });
-
-    if (!user || !(await argon2.verify(user.resetPasswordToken, token))) {
+    console.log('User' ,user);
+    if (!user) {
       return res.status(400).json({ msg: 'Invalid or expired token' });
     }
 
-    user.password = await argon2.hash(req.body.password);
+    console.log("Stored Hashed Token:", user.resetPasswordToken);
+
+    const isMatch = await argon2.verify(user.resetPasswordToken, token);
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Invalid or expired token' });
+    }
+
+    user.password = await argon2.hash(password);
     user.resetPasswordToken = undefined;
     user.resetPasswordExpire = undefined;
     await user.save();
 
     res.json({ msg: 'Password reset successfully' });
   } catch (err) {
+    console.error(err);
     res.status(500).json({ msg: 'Server error' });
   }
 };
+
